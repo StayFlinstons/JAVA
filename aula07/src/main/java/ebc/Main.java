670e916435e3a585739c6360dc845436f9a6a89a
Richard <88545769+StayFlinstons@users.noreply.github.com>
2024-09-27 11:45:25 -0300

    xuli bruli

---
+++ aula07/src/main/java/ebc/Main.java
@@ -0,0 +1,16 @@
+package ebc;
+
+public class Main {
+    public static void main(String[] args) {
+        Lutadores l[] = new Lutadores[6];
+
+        l[0] = new Lutadores("Pretty Boy", "França", 31, 1.75f, 68.91f, 11, 2, 1);
+        l[1] = new Lutadores("Putscript", "Brasil", 29, 1.68f, 57.81f, 14, 2, 3);
+        l[2] = new Lutadores("Snapshadow", "EUA", 35, 1.65f, 80.9f, 12, 2, 1);
+        l[3] = new Lutadores("Dead Code", "Australia", 28, 1.93f, 81.6f, 13, 0, 2);
+        l[4] = new Lutadores("UFOŁobol", "Brasil", 37, 1.70f, 119.31f, 5, 4, 3);
+        l[5] = new Lutadores("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
+
+        l[3].status();
+    }
+}
