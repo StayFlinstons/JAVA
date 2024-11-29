a33d4c7ee8fb3b7b664149d641a2313408706b31
Richard <88545769+StayFlinstons@users.noreply.github.com>
2024-11-29 09:36:59 -0300

    xuli

--- aula13/src/main/java/aula13/Main.java
+++ aula13/src/main/java/aula13/Main.java
@@ -8,15 +8,14 @@ public class Main {
         p[0] = new Pessoa("Marcos", 21, "M");
         p[1] = new Pessoa("Maria", 25, "F");
 
-        l[0] = new Livro("POO na prática", "Allan Stieg", 300, p[0]);
+        l[0] = new Livro("POO na pratica", "Allan Stieg", 300, p[1]);
         l[1] = new Livro("Web Designer", "Marcos Pasquim", 500, p[1]);
         l[2] = new Livro("A volta sem ir", "Jonas Confuso", 800, p[0]);
  
         l[0].abrir();
         l[0].folhear(299);
         l[0].avancarPag();
-        //l[0].avancarPag();
-        //l[0].voltarPag();
+
         System.out.println(l[0].detalhes());
     }
 }
--- aula13/src/main/java/aula13/Pessoa.java
+++ aula13/src/main/java/aula13/Pessoa.java
@@ -11,42 +11,32 @@ public class Pessoa {
         this.sexo = sexo;
     }
 
+    public void fazerAniver(){
+        this.idade++;
+    }
+
     public String getNome() {
         return nome;
     }
-
-
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-
-
     public int getIdade() {
         return idade;
     }
-
-
 
     public void setIdade(int idade) {
         this.idade = idade;
     }
 
-
-
     public String getSexo() {
         return sexo;
     }
 
-
-
     public void setSexo(String sexo) {
         this.sexo = sexo;
     }
-
-    public void fazerAniver(){
-        this.idade++;
-    }
-
+    
 }
